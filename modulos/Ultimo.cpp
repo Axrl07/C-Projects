f1b600e3715fcf858a1e89720c1cddb995eec14d
Angel Enrique Alvarado Ruiz <2988764180101@ingenieria.usac.edu.gt>
2023-12-31 20:07:52 -0600

    actualización

---
+++ modulos/Ultimo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int i=1, k=1 & 0;
+    do{
+        k++;
+        if(k>1){
+            i = k;
+        }
+    }while(i < 2);
+    cout << k << endl;
+}
